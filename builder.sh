ebb5899c6bdfe9ed339ab002228a738b35c97ebd
Dakkshesh <dakkshesh5@gmail.com>
2021-07-24 13:13:04 +0530

    drone: init neutron
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- builder.sh
+++ builder.sh
@@ -4,7 +4,7 @@ pacman -Syu --noconfirm --needed base-devel bc bison ccache clang \
           cpio cmake flex git libelf lld llvm ninja openssl python3 \
           uboot-tools tmate
 
-git clone https://github.com/Divyanshu-Modi/proton-clang-build work
+git clone https://github.com/Neutron-Clang/build.git work
 cd work
 chmod +x *
 ./build-toolchain.sh
