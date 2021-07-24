74235027aad1c679774c89f2bc48b4e55a687268
Divyanshu-Modi <divyan.m05@gmail.com>
2021-07-24 12:45:26 +0530

    Init: AtomX clang builder
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- builder.sh
+++ builder.sh
@@ -1,18 +1,12 @@
 #bin/#!/bin/bash
 
-pacman -Syu --noconfirm --needed git bc inetutils zip libxml2 python3 \
-                                 jre-openjdk jdk-openjdk flex bison libc++ python-pip
+pacman -Syu --noconfirm --needed base-devel bc bison ccache clang \
+          cpio cmake flex git libelf lld llvm ninja openssl python3 \
+          uboot-tools tmate
 
-git clone -q --depth=1 https://github.com/mvaisakh/gcc-arm64 -b  gcc-new $HOME/gcc-arm64
-git clone -q --depth=1 https://github.com/mvaisakh/gcc-arm -b gcc-new $HOME/gcc-arm32
-git clone -q --depth=1 https://github.com/Divyanshu-Modi/AnyKernel3 $HOME/Repack
-git clone -q --depth=1 https://github.com/Divyanshu-Modi/Atom-X-Kernel -b main $HOME/Kernel
-pip3 -q install telegram-send
-echo 'Sources and Api installed...'
+git clone https://github.com/Divyanshu-Modi/proton-clang-build work
+cd work
+chmod +x *
+./build-toolchain.sh
 
-sed -i s/demo1/${BOT_API_KEY}/g telegram-send.conf
-sed -i s/demo2/${CHAT_ID}/g telegram-send.conf
-mkdir $HOME/.config
-mv telegram-send.conf $HOME/.config/telegram-send.conf
-mv build.sh $HOME/Kernel/build.sh
-bash $HOME/Kernel/build.sh
+tmate
